f259fa356c050f0a22776e6b80482a23459a4fe5
aliyun0758874076 <2300202994@qq.com>
2023-07-18 09:07:29 +0800

    小修改

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -5,12 +5,12 @@ import { asyncRouterMap, constantRouterMap } from '@/router'
  * @param perms
  * @param route
  */
+
 function hasPermission(perms, route) {
-  if (route.meta && route.meta.perms) {
-    return perms.some(perm => route.meta.perms.includes(perm))
-  } else {
-    return true
-  }
+	if (route.meta && route.meta.perms) {
+		return perms.some((perm) => route.meta.perms.includes(perm))
+	}
+	return true
 }
 
 /**
@@ -18,52 +18,51 @@ function hasPermission(perms, route) {
  * @param routes asyncRouterMap
  * @param perms
  */
+
 function filterAsyncRouter(routes, perms) {
-  const res = []
+	const res = []
 
-  routes.forEach(route => {
-    const tmp = { ...route }
-    if (tmp.children) {
-      tmp.children = filterAsyncRouter(tmp.children, perms)
-      if (tmp.children && tmp.children.length > 0) {
-        res.push(tmp)
-      }
-    } else {
-      if (hasPermission(perms, tmp)) {
-        res.push(tmp)
-      }
-    }
-  })
+	routes.forEach((route) => {
+		const tmp = { ...route }
+		if (tmp.children) {
+			tmp.children = filterAsyncRouter(tmp.children, perms)
+			if (tmp.children && tmp.children.length > 0) {
+				res.push(tmp)
+			}
+		} else if (hasPermission(perms, tmp)) {
+			res.push(tmp)
+		}
+	})
 
-  return res
+	return res
 }
 
 const permission = {
-  state: {
-    routers: constantRouterMap,
-    addRouters: []
-  },
-  mutations: {
-    SET_ROUTERS: (state, routers) => {
-      state.addRouters = routers
-      state.routers = constantRouterMap.concat(routers)
-    }
-  },
-  actions: {
-    GenerateRoutes({ commit }, data) {
-      return new Promise(resolve => {
-        const { perms } = data
-        let accessedRouters
-        if (perms.includes('*')) {
-          accessedRouters = asyncRouterMap
-        } else {
-          accessedRouters = filterAsyncRouter(asyncRouterMap, perms)
-        }
-        commit('SET_ROUTERS', accessedRouters)
-        resolve()
-      })
-    }
-  }
+	state: {
+		routers: constantRouterMap,
+		addRouters: []
+	},
+	mutations: {
+		SET_ROUTERS: (state, routers) => {
+			state.addRouters = routers
+			state.routers = constantRouterMap.concat(routers)
+		}
+	},
+	actions: {
+		GenerateRoutes({ commit }, data) {
+			return new Promise((resolve) => {
+				const { perms } = data
+				let accessedRouters
+				if (perms.includes('*')) {
+					accessedRouters = asyncRouterMap
+				} else {
+					accessedRouters = filterAsyncRouter(asyncRouterMap, perms)
+				}
+				commit('SET_ROUTERS', accessedRouters)
+				resolve()
+			})
+		}
+	}
 }
 
 export default permission
